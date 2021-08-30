c2aea6bf87e013eed387d8c6b016b816e795d90b
Ignacio Baptista [PC] <ibapista@frba.utn.edu.ar>
2021-08-30 13:38:47 -0300

    Agregado parallax con soporte para rotacion

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -1,35 +1,29 @@
 using UnityEngine;
-
 // ReSharper disable Unity.InefficientPropertyAccess
 
 public class Parallax : MonoBehaviour
 {
     public float parallaxEffect;
     public float xMax = 282;
-    public float speed = 100;
     
-    private Rigidbody2D rigidBody;
+    private const float speed = 200;
+    
+    private Rigidbody rigidBody;
 
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody = GetComponent<Rigidbody>();
+        parallaxEffect = Mathf.Clamp(1 - parallaxEffect, 0, 1);
     }
 
     private void Update()
     {
-        float parallaxEffectValue = Mathf.Clamp(1 - parallaxEffect, 0, 1);
-
-        Vector3 speedVector = transform.right * speed * parallaxEffectValue;
-
-        Debug.Log("Speed vector: " + speedVector);
-        
-        // Establezco una velocidad en base a speed y al efecto paralax
-        rigidBody.velocity = speedVector;
+        // Calculo y aplico el vector velocidad en base a speed y efecto paralax
+        rigidBody.velocity = transform.right * (speed * parallaxEffect);
 
         // Si me pase del borde vuelvo al inicio
-        if (transform.position.x >= xMax)
-            transform.position = new Vector3(0, transform.position.y, transform.position.z);
-
-        //transform.position = new Vector3(startpos + dist, transformPosition.y, transformPosition.z);
+        if (Vector3.Distance(transform.position, Vector3.zero) >= xMax)
+            //transform.localPosition = new Vector3(0, transform.localPosition.y, transform.localPosition.z);
+            transform.position -= transform.right * xMax;
     }
 }
